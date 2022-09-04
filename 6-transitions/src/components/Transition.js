9ff8faad711de5581c10faf27f82b1b12e63ee73
thijsBoet <m.boet@chello.nl>
2022-09-04 18:36:13 +0200

    starting sec 6 transitions

--- 6-transitions/src/components/Transition.js
+++ 6-transitions/src/components/Transition.js
@@ -1,5 +1,35 @@
+import { useState } from 'react';
+import { Transition } from 'react-transition-group';
+
 const TransitionComp = () => {
-  return <div>Transition</div>;
+	const [show, setShow] = useState(true);
+
+	const toggleDiv = () => {
+		setShow(!show);
+	};
+	return (
+		<>
+			{show && (
+				<Transition in={show} timeout={2000}>
+					{state => (
+						<div
+							style={{
+								background: 'red',
+								height: '100px',
+								transition: 'all 2s ease',
+								opacity: state === 'exited' || state === 'exiting' ? 0 : 1,
+							}}
+						>
+							{state}
+						</div>
+					)}
+				</Transition>
+			)}
+			<button className='btn btn-primary mt-3' onClick={toggleDiv}>
+				Toggle show
+			</button>
+		</>
+	);
 };
 
 export default TransitionComp;
