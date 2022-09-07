9f950c126734c3fa0fca57e8ac0ecee31aa16a77
Matthijs Boet <mboet@client13.ict.fqdn>
2022-09-07 15:55:06 +0200

    starting payload in redux

--- 7-redux/src/App.js
+++ 7-redux/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { addMovie } from './store/movies';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const movies = useSelector(state => state.movies.list);
+	const dispatch = useDispatch();
+
+	return (
+		<>
+			<h2>Movies</h2>
+			<ul>
+				{movies &&
+					movies.map(movie => (
+						<li key={movie.id}>{`${movie.title} - ${movie.year}`}</li>
+					))}
+			</ul>
+			<hr />
+			<button onClick={() => dispatch(addMovie())}>Add Movie</button>
+		</>
+	);
+};
 
 export default App;
---
+++ 7-redux/src/store/movies.js
@@ -0,0 +1,47 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+export const moviesSlice = createSlice({
+    name: 'movies',
+    initialState: {
+        list: [
+            {
+                id: 1,
+                title: 'The Shawshank Redemption',
+                year: 1994,
+            },
+            {
+                id: 2,
+                title: 'The Godfather',
+                year: 1972,
+            },
+            {
+                id: 3,
+                title: 'The Godfather: Part II',
+                year: 1974,
+            },
+            {
+                id: 4,
+                title: 'The Dark Knight',
+                year: 2008,
+            },
+            {
+                id: 5,
+                title: 'Top Gun',
+                year: 1986,
+            },
+        ],
+    },
+    reducers: {
+        addMovie: (state) => {
+            const newMovie = {
+                id: state.list.length + 1,
+                title: 'Sunshine',
+                year: 2019,
+            }
+            state.list = [...state.list, newMovie];
+        }
+    }
+});
+
+export const { addMovie } = moviesSlice.actions;
+export default moviesSlice.reducer;
